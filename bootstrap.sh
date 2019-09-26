23b4516c0f5a1e0ccb9311936a5554c009005a59
Theo McCaie <tam203@gmail.com>
2019-09-26 16:41:44 +0100

    wip

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,4 +9,31 @@ ls -l /envs
 
 conda env list
 
-sleep 60
+# 
+
+
+ENV_NAME="export"
+
+## set init conda
+
+# conda init bash
+# source ~/.bashrc
+# This above doesn't work because .bashrc exits early if not in interactive setting.
+eval "$(conda shell.bash hook)"
+
+## create the env
+conda env create -n "$ENV_NAME" -y -f env.yml
+conda activate $ENV_NAME
+conda install zarr -y # and the rest. 
+
+## export it
+EXPORT_TAR=/home/jovyan/"$ENV_NAME".tar.gz
+conda activate base
+conda install -c conda-forge conda-pack -y
+conda pack -n "$ENV_NAME" -o $EXPORT_TAR
+
+## extract to shared storage
+ENV_DEST="/envs/$ENV_BUILD_VERSION"
+rm -r /envs/$ENV_BUILD_VERSION
+mkdir -p $ENV_DEST
+tar -xzf $EXPORT_TAR -C $ENV_DEST
