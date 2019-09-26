7a73353ff8133715290034243b845044a1096fe9
Theo McCaie <tam203@gmail.com>
2019-09-26 19:51:04 +0100

    wip

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,30 +4,27 @@ set -ex
 
 echo "<< Let's go >>"
 
-ls -l /
-ls -l /envs
 
-conda env list
-
-ENV_NAME="export"
+# ENV_NAME="export"
 
 ## set init conda
 
 # conda init bash
 # source ~/.bashrc
 # This above doesn't work because .bashrc exits early if not in interactive setting.
-eval "$(conda shell.bash hook)"
+# eval "$(conda shell.bash hook)"
 
-## create the env
-conda env create -n "$ENV_NAME"  -f env.yml
-
-## export it
-EXPORT_TAR=/home/jovyan/"$ENV_NAME".tar.gz
-conda install -c conda-forge conda-pack -y
-conda pack -n "$ENV_NAME" -o $EXPORT_TAR
-
-## extract to shared storage
+## create the env on shared storage
 ENV_DEST="/envs/$ENV_BUILD_VERSION"
-rm -rf /envs/$ENV_BUILD_VERSION
-mkdir -p $ENV_DEST
-tar -xzf $EXPORT_TAR -C $ENV_DEST
+conda env create -p "$ENV_DEST"  -f env.yml
+
+# ## export it
+# EXPORT_TAR=/home/jovyan/"$ENV_NAME".tar.gz
+# conda install -c conda-forge conda-pack -y
+# conda pack -n "$ENV_NAME" -o $EXPORT_TAR
+
+# ## extract to shared storage
+
+# rm -rf /envs/$ENV_BUILD_VERSION
+# mkdir -p $ENV_DEST
+# tar -xzf $EXPORT_TAR -C $ENV_DEST
