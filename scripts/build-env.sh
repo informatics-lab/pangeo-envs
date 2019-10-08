fe41257d06a5afdf2da26a0ce7fa1db913fb38ce
Theo McCaie <tam203@gmail.com>
2019-10-08 15:17:00 +0100

    edit the template to  actually trigger build. more logging in build script

--- scripts/build-env.sh
+++ scripts/build-env.sh
@@ -7,7 +7,16 @@ TMP_DIR="${BASE_DIR}/${TMP_ID}"
 DEST_DIR="${BASE_DIR}/${ENV_NAME}"
 PRE_DELETE_DIR="${DEST_DIR}.tmp"
 
+echo "*** Create env in tmp location: $TMP_DIR"
 conda env create -f env.yaml -p $TMP_DIR
+
+echo "*** Move existing env if exists: $DEST_DIR"
 mv $DEST_DIR ${PRE_DELETE_DIR} || echo "$DEST_DIR doesn't exist"
+
+echo "*** Rename env from temp location to dest location"
 mv $TMP_DIR $DEST_DIR
-rm -rf $PRE_DELETE_DIR
+
+echo "*** Remove old tmp env if exist"
+rm -rf $PRE_DELETE_DIR || echo " $PRE_DELETE_DIR doesn't exist"
+
+echo "** Done: ${ENV_NAME} at ${DEST_DIR}"
